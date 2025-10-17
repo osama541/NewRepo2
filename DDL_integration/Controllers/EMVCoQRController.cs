dc8d076450d94db9347d1e348070f9686fa72670
osama541 <ro6760644@gmail.com>
2025-10-17 01:34:42 -0700

    Add project files.

---
+++ DDL_integration/Controllers/EMVCoQRController.cs
@@ -0,0 +1,35 @@
+﻿using EmvQrCore;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace DDL_integration.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EMVCoQRController : ControllerBase
+    {
+        [HttpPost("generate")]
+        public IActionResult GenerateQr([FromBody] JsonElement request)
+        {
+            try
+            {
+                var tags = request.GetProperty("tags").Deserialize<Dictionary<string, object>>();
+                var isImageRequired = request.GetProperty("isImageRequired").GetBoolean();
+
+                var result = EmvQrUtility.Generate(tags, isImageRequired);
+
+                return Ok(new
+                {
+                    QrString = result.qrString,
+                    Base64Png = result.base64Png,
+                    Base64Svg = result.base64Svg
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { error = ex.Message });
+            }
+        }
+    }
+}
